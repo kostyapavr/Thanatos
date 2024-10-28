eff3ebcee32556a4eb932bc11a77b38b8d88205c
spad17 <mirgorodsky.alexey@gmail.com>
2024-10-28 18:34:24 +0300

    add level link

--- Thanatos/Assets/Scripts/LevelController.cs
+++ Thanatos/Assets/Scripts/LevelController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const int maxLevels = 5;
+    private int[] maxRooms = new int[maxLevels] { 4, 5, 6, 8, 10 };
+
     void Start()
     {
         DontDestroyOnLoad(this);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(getNextSceneName(currentSceneName));
+        }
+    }
+
+    string getNextSceneName(string currentName)
+    {
+        var splitResult = currentName.Split('.');
+        int currentLevel = int.Parse(splitResult[0].Last().ToString());
+        int currentRoom = int.Parse(splitResult.Last());
+
+        int nextLevel = 1;
+        int nextRoom = (currentRoom + 1) % maxRooms[currentLevel];
+        nextLevel = (nextRoom == 0) ? currentLevel + 1 : currentLevel;
+        nextRoom = (nextRoom == 0) ? 1 : nextRoom;
         
+        string nextSceneName = $"Level{nextLevel}.{nextRoom}";
+        return nextSceneName;
     }
 }
