171ba914a1d460a9aad0508460d501cefeffa286
kostyapavr <kostya.pavlenko05@gmail.com>
2024-10-31 18:14:09 +0300

    Added player movement

--- Thanatos/Assets/Scripts/PlayerMovement.cs
+++ Thanatos/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 MovementSpeed = new Vector2(100.0f, 100.0f);
+    private new Rigidbody2D rb;
+    private Vector2 inputVector = new Vector2(0.0f, 0.0f);
+
+    void Awake()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity += inputVector * 10;
     }
 }
