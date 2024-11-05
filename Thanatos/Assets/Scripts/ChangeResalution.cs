73186987474be744367b3dfa90e3209f1ef9c174
Nikita Manov <nikitamanov49@gmail.com>
2024-11-05 22:23:17 +0300

    Create-MainSettings

---
+++ Thanatos/Assets/Scripts/ChangeResalution.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeResalution : MonoBehaviour
+{
+    public Dropdown Dropdown;
+
+    public void Change()
+    {
+        if(Dropdown.value == 0)
+        {
+            Screen.SetResolution(800, 600, true);
+        }
+        else if (Dropdown.value == 1)
+        {
+           Screen.SetResolution(1024, 768, true);
+        }
+        else if (Dropdown.value == 2)
+        {
+            Screen.SetResolution(1366, 768, true);
+        }
+        else if (Dropdown.value == 3)
+        {
+            Screen.SetResolution(1920, 1080, true);
+        }
+    }
+
+
+}
