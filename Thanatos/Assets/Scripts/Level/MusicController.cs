a344949acf823a3d82c45f82f043ec655f16a161
Алексей Миргородский <mirgorodsky.alexey@gmail.com>
2025-03-29 14:48:24 +0300

    update music player
    update

--- Thanatos/Assets/Scripts/Level/MusicController.cs
+++ Thanatos/Assets/Scripts/Level/MusicController.cs
@@ -7,6 +7,8 @@ public class MusicController : MonoBehaviour
 {
     public AudioClip menuClip;
     public AudioClip level1;
+    public AudioClip level2;
+    public AudioClip level3;
     public AudioSource audioSource;
 
     void Awake()
@@ -19,12 +21,26 @@ public class MusicController : MonoBehaviour
     {
         if (arg0.buildIndex <= 1 && audioSource.clip != menuClip)
         {
+            audioSource.Stop();
             audioSource.clip = menuClip;
             audioSource.Play();
         }
-        else if (arg0.buildIndex > 1 && audioSource.clip != level1)
+        else if (arg0.buildIndex > 1 && arg0.buildIndex <= 6 && audioSource.clip != level1)
         {
+            audioSource.Stop();
             audioSource.clip = level1;
+            audioSource.Play();
+        }
+        else if (arg0.buildIndex > 6 && arg0.buildIndex <= 11 && audioSource.clip != level2)
+        {
+            audioSource.Stop();
+            audioSource.clip = level2;
+            audioSource.Play();
+        }
+        else if (arg0.buildIndex > 11 && audioSource.clip != level3)
+        {
+            audioSource.Stop();
+            audioSource.clip = level3;
             audioSource.Play();
         }
     }
