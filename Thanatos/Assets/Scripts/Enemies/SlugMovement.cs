7ef9d6cece1ff6da41057b4a494293a8afc86bc0
TheSoldierOfPunisher <akademegex36512@gmail.com>
2025-04-30 21:46:20 +0300

    adding movement for the slug

---
+++ Thanatos/Assets/Scripts/Enemies/SlugMovement.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
+
+public class SlugMovement : Enemy
+{
+    public float minHealth;
+    public new float maxHealth;
+
+    public float attackIntervalMin;
+    public float attackIntervalMax;
+
+    public float damage;
+
+    public float moveSpeedMin;
+    public float moveSpeedMax;
+
+    public float obstacleDetectSize;
+
+    [HideInInspector] public Transform player;
+
+    private float detectionDistance = 6f;
+
+    public float attackRange;
+    private float chaseRange = 60f;
+
+    private float lastAttackTime;
+
+    private float health;
+    private float attackInterval;
+    private float moveSpeed;
+
+    private int obstacleMask;
+
+    public Sprite firstSprite;
+    public Sprite secondSprite;
+    private int animInd = 0;
+
+    public float walkAnimTime;
+
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private GameObject slowAreaPrefab;
+    [SerializeField] private float spawnInterval;
+    [SerializeField] private float lifeTime;
+
+    private float _timer;
+
+    public override void Start()
+    {
+        RandomiseProperties();
+
+        currentHealth = health;
+        //InvokeRepeating("Attack", attackInterval, attackInterval);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        obstacleMask = LayerMask.GetMask("Obstacle");
+    }
+
+    private void Attack()
+    {
+        if (Time.time >= lastAttackTime + attackInterval)
+        {
+            Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(transform.position, attackRange);
+            foreach (var hit in hitPlayer)
+            {
+                if (hit.CompareTag("Player"))
+                {
+                    Player player = hit.GetComponent<Player>();
+                    if (player != null)
+                    {
+                        player.TakeDamage(damage, gameObject, DamageEffects.Nothing);
+                    }
+                }
+            }
+            lastAttackTime = Time.time;
+        }
+    }
+
+    public override void FixedUpdate()
+    {
+        if (player != null)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+            if (distanceToPlayer <= attackRange)
+            {
+                Attack();
+            }
+            else if (distanceToPlayer <= chaseRange)
+            {
+                _timer += Time.deltaTime;
+
+                if (_timer >= spawnInterval)
+                {
+                    SpawnTrail();
+                    _timer = 0f;
+                }
+
+                ChasePlayer();
+            }
+        }
+    }
+
+    private float delayTime = 0.0f;
+    private void ChasePlayer()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, obstacleDetectSize, direction, detectionDistance, obstacleMask);
+        if (hit.collider != null && !hit.collider.GetComponent<Player>())
+        {
+            Vector2 newDirection = direction + Vector2.Perpendicular(direction) * 2;
+            transform.position = Vector2.MoveTowards(transform.position, (Vector2)transform.position + newDirection, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+
+        if (firstSprite == null || secondSprite == null) return;
+
+        if (delayTime < walkAnimTime) delayTime += Time.deltaTime;
+        else if (delayTime >= walkAnimTime)
+        {
+            if (animInd == 0)
+            {
+                spriteRenderer.sprite = firstSprite;
+                animInd = 1;
+            }
+            else
+            {
+                spriteRenderer.sprite = secondSprite;
+                animInd = 0;
+            }
+            delayTime = 0.0f;
+        }
+    }
+
+    void SpawnTrail()
+    {
+        GameObject trail = Instantiate(slowAreaPrefab, transform.position, Quaternion.identity);
+        Destroy(trail, lifeTime);
+    }
+
+    private IEnumerator FadeTrail(SpriteRenderer renderer, float duration)
+    {
+        float timer = 0f;
+        Color startColor = renderer.color;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, timer / duration);
+            renderer.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            yield return null;
+        }
+    }
+
+    private void RandomiseProperties()
+    {
+        health = Random.Range(minHealth, maxHealth);
+        attackInterval = Random.Range(attackIntervalMin, attackIntervalMax);
+        moveSpeed = Random.Range(moveSpeedMin, moveSpeedMax);
+    }
+}
